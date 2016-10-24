b90122df5ab17cadf858d29b7a2820e4e81b6fff
Miguel Moral <miguelmoralllamas@correo.ugr.es>
2016-10-25 01:21:46 +0200

    Añadiendo algún test en test.py

--- empresa/tests.py
+++ empresa/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+import datetime
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Datos
+
+class TestIntroduccionValores(TestCase):
+
+    def test_formulario(self):
+        e = Datos(nombre_empresa = "Empresatest", persona = "Personaprueba", pub_date = timezone.now(), puntuacion = "5")
+        e.save()
+        self.assertEqual(e.nombre_empresa, "Empresatest")
